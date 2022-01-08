8b213c1a72a1fcbe2258121c566a5ff51966e3b4
Moritz Ruëff <93935623+MoritzRueff@users.noreply.github.com>
2022-01-08 16:47:19 +0100

    initial commit

---
+++ capstone-project/src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import './App.css'
+
+export default function App() {
+
+  return (
+    <Container>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  max-width: 26rem;
+  margin: 0 auto;
+  border: 1px solid-black;
+  background-color: var(--primary);
+  color: white;  
+  border-radius: 15px;
+`;
